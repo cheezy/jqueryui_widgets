0e63be485a5ec74c904238102072aa3a8b3a283c
Paul-Clewell <andrevarshea@gmail.com>
2013-03-19 23:01:09 -0400

    Documentation on basic dialog and progress bar classes.

--- lib/jqueryui_widgets/basic_dialog.rb
+++ lib/jqueryui_widgets/basic_dialog.rb
@@ -1,13 +1,30 @@
+
+#
+# Basic Dialog allows JQuery UI Widgets to interact
+# with the basic dialogs that JQueryUI pops up.
+#
 class JQueryUIWidgets::BasicDialog < PageObject::Elements::Div
 
+  #
+  # The title function grabs the span element of the title
+  # bar in the UI dialog and returns the text.
+  #
   def title
     div_element(:class => 'ui-dialog-titlebar').span_element.text
   end
 
+  #
+  # Content function will return the entire text of
+  # the JQuery UI dialog box.
+  #
   def content
     div_element(:class => 'ui-dialog-content').text
   end
 
+  #
+  # Close function will, of course, click the button
+  # labeled close.
+  #
   def close
     button_element(:class => 'ui-dialog-titlebar-close').click
   end
--- lib/jqueryui_widgets/progress_bar.rb
+++ lib/jqueryui_widgets/progress_bar.rb
@@ -1,13 +1,35 @@
+
+#
+# The progress bar class will interact with the
+# JQuery UI Widget Progress Bar and allow for
+# returning the Minimum value, Maximum Value and
+# Current progress.
+#
 class JQueryUIWidgets::ProgressBar < PageObject::Elements::Div
 
+  #
+  # The minimum function returns the minimum
+  # possible value of the progress bar by
+  # returning the 'aria-valuemin' attribute's
+  # value.
+  #
   def minimum
     convert_to_number attribute('aria-valuemin')
   end
 
+  #
+  # The maximum function returns the maximum
+  # value of the progress bar by returning
+  # the 'aria-valuemax' attribute's value.
   def maximum
     convert_to_number attribute('aria-valuemax')
   end
 
+  #
+  # The Current function returns the current
+  # value of the progress bar by returning the
+  # 'aria-valuenow' attribute's value.
+  #
   def current
     convert_to_number attribute('aria-valuenow')
   end
