bbb515dafed05282bfde620a65827f43d7677220
Paul Clewell <andrevarshea@gmail.com>
2013-04-08 17:54:04 -0400

    Naming changes.

--- lib/jqueryui_widgets/slider.rb
+++ lib/jqueryui_widgets/slider.rb
@@ -2,7 +2,7 @@
 class JQueryUIWidgets::Slider < PageObject::Elements::Div
 
   def self.accessor_methods(accessor, name)
-    accessor.send :define_method, "#{name}_current" do
+    accessor.send :define_method, "#{name}" do
       slider = self.send "#{name}_element"
       value = slider.link_element(:class => 'ui-slider-handle').style('left')
       if value.include? ('px')
