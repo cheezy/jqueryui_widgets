e163db0d4d181dd5a69eac680338cdd3f3e69f71
Paul-Clewell <andrevarshea@gmail.com>
2013-06-03 09:53:23 -0400

    Finishing the Datepicker functionality, as well as version bump.

--- lib/jqueryui_widgets/datepicker.rb
+++ lib/jqueryui_widgets/datepicker.rb
@@ -1,5 +1,51 @@
+
+#
+# Class that wraps around the Datepicker widget, allowing for the
+# user to interact with single or multiple Datepicker widgets, as
+# long as only a single Datepicker is active.
+#
+# NOTE: We've kept to the basic functionality of the Datepicker
+# widget, allowing for extendability of the class, and to avoid
+# any difficulty by focusing on minutiae.
+#
+
 class JQueryUIWidgets::Datepicker < PageObject::Elements::TextField
 
+  #
+  # Generates nine methods.
+  #
+  # The {NAME}= method allows the user to set the value
+  # of the text field that generates the Datepicker widget.
+  #
+  # The {NAME} method returns the value of the Datepicker text field.
+  #
+  # The {NAME}_select_day method allows the user to pass in a variable to
+  # search through the Datepicker widget table and click on that variable,
+  # in this case the day.
+  #
+  # @params: [variable] the day to find.
+  #
+  # @example:
+  #    datepicker_select_day '10'
+  #     Will search through the datepicker table and click on the 10th
+  #     day of the month.
+  #
+  # The {NAME}_next_month method will click the next month arrow,
+  # setting the datepicker widget to the next month.
+  #
+  # The {NAME}_previous_month method will click the previous month
+  # arrow, setting the datepicker widget to the previous month.
+  #
+  # The {NAME}_day method will search through the datepicker table
+  # and return the day with the class of 'ui-state-active', which is
+  # the current day according to the widget.
+  #
+  # The {NAME}_month method will return the month header of the
+  # datepicker table, and return the current month.
+  #
+  # The {NAME}_year method will return the year header of the
+  # datepicker table, and return the current year.
+  #
   def self.accessor_methods(accessor, name)
     accessor.send :define_method, "#{name}=" do |date|
       self.send("#{name}_element").value = date
